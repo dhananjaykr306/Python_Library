d63dfa7ed0b99afc6fff779eeebb25ef79186ac7
Dhananjay kumar <107258319+dhananjaykr306@users.noreply.github.com>
2024-11-21 09:44:09 +0530

    [Dhananjay] add :1. Read excel file

---
+++ Pandas/13-PandasReadExcel/Demo1.py
@@ -0,0 +1,12 @@
+# Read an excel file in pandas
+# code by Dhananjay
+
+import pandas as pd
+
+# input the Excel file
+# Load the excel file
+
+df = pd.read_excel("odi.xlsx")
+
+# Display the Dataframe
+print("DataFrame : \n",df)
